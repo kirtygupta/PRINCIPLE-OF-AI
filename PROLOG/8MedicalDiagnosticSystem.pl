e5474f63ae0465a408d3bb9c65a8bb5ee0c14834
KIRTY GUPTA <146504625+kirtygupta@users.noreply.github.com>
2023-12-16 11:34:57 +0530

    Update 8MedicalDiagnosticSystem.pl

--- PROLOG/8MedicalDiagnosticSystem.pl
+++ PROLOG/8MedicalDiagnosticSystem.pl
@@ -1,8 +1,14 @@
-%Medical diagnostic system in prolog
 symptom(charlie,fever).
 symptom(charlie,rash).
 symptom(charlie,headache).
 symptom(charlie,runny_nose).
-hypothesis(Patient,measles):-symptom(Patient,fever),symptom(Patient,cough),symptom(Patient,conjunctivites),symptom(Patient,runny_nose),symptom(Patient,rash).
+
+hypothesis(Patient,measles):-symptom(Patient,fever),symptom(Patient,cough),symptom(Patient,conjunctivities),symptom(Patient,runny_nose),symptom(Patient,rash).
+
 hypothesis(Patient,german_measles):-symptom(Patient,fever),symptom(Patient,headache),symptom(Patient,runny_nose),symptom(Patient,rash).
+
 hypothesis(Patient,flu):-symptom(Patient,fever),symptom(Patient,headache),symptom(Patient,body_ache),symptom(Patient,conjunctivities),symptom(Patient,chills),symptom(Patient,sore_throat),symptom(Patient,cough),symptom(Patient,runny_nose).
+
+hypothesis(Patient,common_cold):-symptom(Patient,headache),symptom(Patient,sneezing),symptom(Patient,sore_throat),symptom(Patient,chills),symptom(Patient,runny_nose).hypothesis(Patient,mumps):-symptom(Patient,fever),symptom(Patient,swollen_glands).
+
+hypothesis(Patient,chicken_pox):-symptom(Patient,fever),symptom(Patient,rash),symptom(Patient,body_ache),symptom(Patient,chills).hypothesis(Patient,whooping_cough):-symptom(Patient,cough),symptom(Patient,sneezing),symptom(Patient,runny_nose).
